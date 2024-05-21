a62a070ab9701a46ae0f3842705f1c34b6e34d8e
medev-lennovo <mahdyarmojtahed@gmail.com>
2024-05-21 14:55:22 +0330

    Init

---
+++ App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) {
+
+        float f1 = 12.3f;
+        float f2 = 12412.51f;
+        float f3 = 13212.512421f;
+
+        System.out.println(f3);
+        System.out.println(f2);
+        System.out.println(f1);
+
+        double d1 = 123123;
+        double d2 = 124123.1231;
+        double d3 = 512132.5343423;
+
+        System.out.println(d3);
+        System.out.println(d2);
+        System.out.println(d1);
+
+    }
+}
